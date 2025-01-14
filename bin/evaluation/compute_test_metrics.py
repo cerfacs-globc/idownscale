5103ec56fc8156ddff4a8e4f17ed391e2161cc2d
zoeleila <zoeleila10@gmail.com>
2025-01-14 15:30:46 +0100

    summer and winter metrics

--- bin/evaluation/compute_test_metrics.py
+++ bin/evaluation/compute_test_metrics.py
@@ -13,7 +13,7 @@ from torchmetrics import MeanSquaredError, PearsonCorrCoef
 
 from iriscc.lightning_module import IRISCCLightningModule
 from iriscc.transforms import MinMaxNormalisation, LandSeaMask, Pad, FillMissingValue
-from iriscc.settings import DATASET_EXP1_DIR, DATES_TEST, GRAPHS_DIR, TARGET_SIZE, RUNS_DIR, METRICS_DIR
+from iriscc.settings import DATASET_EXP1_DIR, DATES_TEST, GRAPHS_DIR, TARGET_SIZE, RUNS_DIR, METRICS_DIR, DATASET_EXP1_30Y_DIR
 from iriscc.transforms import UnPad
 
 exp = str(sys.argv[1]) # ex : exp 1
@@ -42,22 +42,33 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 rmse = MeanSquaredError(squared=False).to(device)
 corr = PearsonCorrCoef().to(device)
 
-rmse_daily = []  
+rmse_temporal = []  
 rmse_spatial = []  
+rmse_spatial_summer = []
+rmse_spatial_winter = []
 bias_spatial = []
+bias_spatial_summer = []
+bias_spatial_winter = []
 corr_spatial = []
+corr_spatial_summer = []
+corr_spatial_winter = []
 y_temporal = []
 y_hat_temporal = []
-corr_temporal = []
+i_summer = []
+i_winter = []
 
 startdate = DATES_TEST[0].date().strftime('%d/%m/%Y')
 enddate = DATES_TEST[-1].date().strftime('%d/%m/%Y')
 period = f'{startdate} - {enddate}'
 
-for date in DATES_TEST:
+for i, date in enumerate(DATES_TEST):
     print(date)
+    if date.month in [6,7,8]:
+        i_summer.append(i)
+    if date.month in [1,2,12]:
+        i_winter.append(i)
     date_str = date.date().strftime('%Y%m%d')
-    sample = glob.glob(str(DATASET_EXP1_DIR/f'sample_{date_str}.npz'))[0]
+    sample = glob.glob(str(DATASET_EXP1_30Y_DIR/f'sample_{date_str}.npz'))[0]
     data = dict(np.load(sample), allow_pickle=True)
     x, y = data['x'], data['y']
     condition = np.isnan(y[0])
@@ -83,31 +94,63 @@ for date in DATES_TEST:
         rmse_spatial += error_squared
         bias_spatial += error
 
+    if date.month in [6,7,8]:
+        if len(rmse_spatial_summer) == 0:
+            rmse_spatial_summer = error_squared
+            bias_spatial_summer = error
+        else:
+            rmse_spatial_summer += error_squared
+            bias_spatial_summer += error
+    elif date.month in [1,2,12]:
+        if len(rmse_spatial_winter) == 0:
+            rmse_spatial_winter = error_squared
+            bias_spatial_winter = error
+        else:
+            rmse_spatial_winter += error_squared
+            bias_spatial_winter += error
+
     ## temporal metrics
     y, y_hat = torch.tensor(y), torch.tensor(y_hat)
     y_flat, y_hat_flat = y[~torch.isnan(y)].to(device), y_hat[~torch.isnan(y_hat)].to(device)
     rmse_value = rmse(y_hat_flat, y_flat).item()
     corr_value = corr(y_hat_flat - torch.mean(y_hat_flat), y_flat - torch.mean(y_flat)).item()
-    rmse_daily.append(rmse_value)
+    rmse_temporal.append(rmse_value)
     corr_spatial.append(corr_value)
 
     y_temporal.append(y_flat)
     y_hat_temporal.append(y_hat_flat)
 
+
 y_temporal, y_hat_temporal = torch.stack(y_temporal), torch.stack(y_hat_temporal)
 corr_temporal = [corr(y_hat_temporal[:,i], y_temporal[:,i]).cpu() for i in range(y_temporal.size(dim=1))]
 corr_temporal = np.stack(corr_temporal)
+corr_temporal_summer = [corr(y_hat_temporal[:,i], y_temporal[:,i]).cpu() for i in i_summer]
+corr_temporal = np.stack(corr_temporal_summer)
+corr_temporal_winter = [corr(y_hat_temporal[:,i], y_temporal[:,i]).cpu() for i in i_winter]
+corr_temporal = np.stack(corr_temporal_winter)
+
+
 rmse_spatial = np.sqrt(rmse_spatial / len(DATES_TEST))
+rmse_spatial_summer = np.sqrt(rmse_spatial_summer / len(i_summer))
+rmse_spatial_winter = np.sqrt(rmse_spatial_winter / len(i_winter))
 bias_spatial = bias_spatial / len(DATES_TEST)
+bias_spatial_summer = bias_spatial_summer / len(i_summer)
+bias_spatial_winter = bias_spatial_winter / len(i_winter)
+
+rmse_temporal_summer = [rmse_temporal[i] for i in i_summer]
+rmse_temporal_winter = [rmse_temporal[i] for i in i_winter]
+corr_spatial_summer = [corr_spatial[i] for i in i_summer]
+corr_spatial_winter = [corr_spatial[i] for i in i_winter]
 
 # Scalars
-d= {'rmse_daily_mean' : [np.mean(rmse_daily)],
-    'rmse_spatial_mean' : [np.nanmean(rmse_spatial)],
-    'bias_spatial_mean' : [np.nanmean(bias_spatial)],
-    'corr_spatial_mean' : [np.mean(corr_spatial)],
-    'corr_temporal_mean' : [np.mean(corr_temporal)]}
+d = {'rmse_temporal_mean' : [np.mean(rmse_temporal), np.mean(rmse_temporal_summer), np.mean(rmse_temporal_winter)],
+    'rmse_spatial_mean' : [np.nanmean(rmse_spatial),np.nanmean(rmse_spatial_summer), np.nanmean(rmse_spatial_winter)],
+    'bias_spatial_mean' : [np.nanmean(bias_spatial),np.nanmean(bias_spatial_summer), np.nanmean(bias_spatial_winter)],
+    'bias_spatial_std' : [np.nanstd(bias_spatial),np.nanstd(bias_spatial_summer), np.nanstd(bias_spatial_winter)],
+    'corr_spatial_mean' : [np.mean(corr_spatial), np.mean(corr_spatial_summer), np.mean(corr_spatial_winter)],
+    'corr_temporal_mean' : [np.mean(corr_temporal), np.mean(corr_temporal_summer), np.mean(corr_temporal_winter)]}
 
-df = pd.DataFrame(d)
+df = pd.DataFrame(d, index = ['all', 'summer', 'winter'])
 df.to_csv(metric_dir/f'metrics_test_mean_{exp}_{test_name}.csv')
 print(df)
 
@@ -139,11 +182,11 @@ plt.savefig(f"{graph_dir}/spatial_bias_distribution.png")
 
 # Temporal distribution
 ## monthly RMSE
-df_rmse = pd.DataFrame({'date': DATES_TEST, 'rmse_daily': rmse_daily})
+df_rmse = pd.DataFrame({'date': DATES_TEST, 'rmse_temporal': rmse_temporal})
 df_rmse['month'] = pd.to_datetime(df_rmse['date']).dt.month
 df_rmse['year'] = pd.to_datetime(df_rmse['date']).dt.year
-rmse_monthly_mean = df_rmse.groupby('month')['rmse_daily'].mean()
-rmse_per_year = df_rmse.pivot_table(index='month', columns='year', values='rmse_daily')
+rmse_monthly_mean = df_rmse.groupby('month')['rmse_temporal'].mean()
+rmse_per_year = df_rmse.pivot_table(index='month', columns='year', values='rmse_temporal')
 plt.figure(figsize=(10, 6))
 plt.suptitle(f'{arch} ({test_name} config)', fontsize=16)
 ax = plt.gca()
@@ -158,7 +201,7 @@ plt.ylabel('RMSE (K)')
 plt.xlabel('Month')
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.legend(loc='upper right', fontsize=12, ncol=2)
-ax.text(0.02, 0.10, f"Mean temporal RMSE: {np.mean(rmse_daily):.2f}", transform=ax.transAxes, fontsize=12, 
+ax.text(0.02, 0.10, f"Mean temporal RMSE: {np.mean(rmse_temporal):.2f}", transform=ax.transAxes, fontsize=12, 
         verticalalignment='top', horizontalalignment='left', color = 'red')
 plt.tight_layout()
 plt.savefig(f"{graph_dir}/monthly_rmse_cycle.png") 
