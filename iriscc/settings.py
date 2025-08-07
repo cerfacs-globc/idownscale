6816c17c746488d0288dd4e17a9ce38614dd2eeb
zoeleila <zoeleila10@gmail.com>
2025-08-07 14:52:05 +0200

    flake8 extend ignore rules

--- iriscc/settings.py
+++ iriscc/settings.py
@@ -59,131 +59,131 @@ PREDICTION_DIR = Path('/scratch/globc/garcia/prediction/')
 CONFIG = {
     'exp3':
         {'target':'safran',
-        'domain': [-6., 12., 40., 52.],
-        'domain_xy' : [60000, 1196000, 1617000, 2681000],
-        'data_projection' : ccrs.LambertConformal(central_longitude=2.337229,
-                                 central_latitude=46.8,
-                                 false_easting=600000,
-                                 false_northing=2200000,
-                                 standard_parallels=(45.89892, 47.69601)),
-        'fig_projection' :ccrs.LambertConformal(central_latitude=46., central_longitude=2.),
-        'pyproj_projection' : pyproj.Proj("+proj=lcc +lon_0=2.337229 +lat_0=46.8 +lat_1=45.89892 +lat_2=47.69601 +x_0=600000 +y_0=2200000"),
-        'shape' : (134,143),
-        'target_file' : OROG_SAFRAN_FILE,
-        'orog_file' : OROG_SAFRAN_FILE, 
-        'dataset' : DATASET_EXP3_30Y_DIR,
-        'target_vars': ['tas'],
-        'input_vars': ['elevation', 'tas'],
-        'channels': ['elevation', 'tas input', 'tas target']
-        },
+            'domain': [-6., 12., 40., 52.],
+            'domain_xy' : [60000, 1196000, 1617000, 2681000],
+            'data_projection' : ccrs.LambertConformal(central_longitude=2.337229,
+                                    central_latitude=46.8,
+                                    false_easting=600000,
+                                    false_northing=2200000,
+                                    standard_parallels=(45.89892, 47.69601)),
+            'fig_projection' :ccrs.LambertConformal(central_latitude=46., central_longitude=2.),
+            'pyproj_projection' : pyproj.Proj("+proj=lcc +lon_0=2.337229 +lat_0=46.8 +lat_1=45.89892 +lat_2=47.69601 +x_0=600000 +y_0=2200000"),
+            'shape' : (134,143),
+            'target_file' : OROG_SAFRAN_FILE,
+            'orog_file' : OROG_SAFRAN_FILE, 
+            'dataset' : DATASET_EXP3_30Y_DIR,
+            'target_vars': ['tas'],
+            'input_vars': ['elevation', 'tas'],
+            'channels': ['elevation', 'tas input', 'tas target']
+            },
 
     'exp4': # obsolete, use exp5 
         {'target':'eobs',
-        'domain': 
-            {'france': [-6., 10., 38, 54],
-             'europe' : [-12.5, 27.5, 31., 71.],
-             'tchequie' : [11.5, 19.5, 45.75, 53.75]},
-        'data_projection' : ccrs.PlateCarree(),
-        'fig_projection' : 
-            {'france' : ccrs.LambertConformal(central_latitude=46., central_longitude=2.),
-             'europe' : ccrs.LambertConformal(central_latitude=51., central_longitude=7.5),
-             'tchequie' : ccrs.LambertConformal(central_latitude=45.75, central_longitude=11.5)},
-        'shape': 
-                {'france': (64,64),
-                'europe' : (160,160),
-                'tchequie' : (32,32)},
-        'target_file' : EOBS_RAW_DIR / 'tas_ens_mean_1d_025deg_reg_v29_0e_19500101-20231231.nc',
-        'orog_file' : EOBS_RAW_DIR / 'elevation_ens_025deg_reg_v29_0e_france.nc',
-        'dataset' : DATASET_EXP4_30Y_DIR,
-        'target_vars': ['tas'],
-        'input_vars': ['elevation', 'tas'],
-        'channels': ['elevation', 'tas input', 'tas target']       
-        },
+            'domain': 
+                {'france': [-6., 10., 38, 54],
+                'europe' : [-12.5, 27.5, 31., 71.],
+                'tchequie' : [11.5, 19.5, 45.75, 53.75]},
+            'data_projection' : ccrs.PlateCarree(),
+            'fig_projection' : 
+                {'france' : ccrs.LambertConformal(central_latitude=46., central_longitude=2.),
+                'europe' : ccrs.LambertConformal(central_latitude=51., central_longitude=7.5),
+                'tchequie' : ccrs.LambertConformal(central_latitude=45.75, central_longitude=11.5)},
+            'shape': 
+                    {'france': (64,64),
+                    'europe' : (160,160),
+                    'tchequie' : (32,32)},
+            'target_file' : EOBS_RAW_DIR / 'tas_ens_mean_1d_025deg_reg_v29_0e_19500101-20231231.nc',
+            'orog_file' : EOBS_RAW_DIR / 'elevation_ens_025deg_reg_v29_0e_france.nc',
+            'dataset' : DATASET_EXP4_30Y_DIR,
+            'target_vars': ['tas'],
+            'input_vars': ['elevation', 'tas'],
+            'channels': ['elevation', 'tas input', 'tas target']       
+            },
     'exp5':
         {'target':'eobs',
-        'domain': [-6., 10., 38, 54],
-        'data_projection' : ccrs.PlateCarree(),
-        'fig_projection' : ccrs.LambertConformal(central_latitude=46., central_longitude=2.),
-        'pyproj_projection' : None,
-        'shape': (64,64),
-        'target_file' : TARGET_EOBS_FRANCE_FILE,
-        'orog_file' : OROG_EOBS_FRANCE_FILE,
-        'dataset' : DATASET_EXP5_30Y_DIR,
-        'target_vars': ['tas'],
-        'input_vars': ['elevation', 'tas'],
-        'channels': ['elevation', 'tas input', 'tas target'],
-        'ssp': 'ssp585'        
-        },
+            'domain': [-6., 10., 38, 54],
+            'data_projection' : ccrs.PlateCarree(),
+            'fig_projection' : ccrs.LambertConformal(central_latitude=46., central_longitude=2.),
+            'pyproj_projection' : None,
+            'shape': (64,64),
+            'target_file' : TARGET_EOBS_FRANCE_FILE,
+            'orog_file' : OROG_EOBS_FRANCE_FILE,
+            'dataset' : DATASET_EXP5_30Y_DIR,
+            'target_vars': ['tas'],
+            'input_vars': ['elevation', 'tas'],
+            'channels': ['elevation', 'tas input', 'tas target'],
+            'ssp': 'ssp585'        
+            },
     'exp6':
         {'target':'eobs',
-        'domain': [-6., 10., 38, 54],
-        'data_projection' : ccrs.PlateCarree(),
-        'fig_projection' : ccrs.LambertConformal(central_latitude=46., central_longitude=2.),
-        'pyproj_projection' : None, # for curvilign grids conservative interpolation
-        'shape': (64,64),
-        'target_file' : TARGET_EOBS_FRANCE_FILE, # target grid coordinates
-        'orog_file' : OROG_EOBS_FRANCE_FILE,
-        'dataset' : DATASET_EXP6_30Y_DIR,
-        'target_vars': ['pr'],
-        'input_vars': ['elevation', 'pr'],
-        'channels': ['elevation', 'pr input', 'pr target'], # to not get lost for normalization
-        'ssp' : 'ssp585'   
-        },
+            'domain': [-6., 10., 38, 54],
+            'data_projection' : ccrs.PlateCarree(),
+            'fig_projection' : ccrs.LambertConformal(central_latitude=46., central_longitude=2.),
+            'pyproj_projection' : None, # for curvilign grids conservative interpolation
+            'shape': (64,64),
+            'target_file' : TARGET_EOBS_FRANCE_FILE, # target grid coordinates
+            'orog_file' : OROG_EOBS_FRANCE_FILE,
+            'dataset' : DATASET_EXP6_30Y_DIR,
+            'target_vars': ['pr'],
+            'input_vars': ['elevation', 'pr'],
+            'channels': ['elevation', 'pr input', 'pr target'], # to not get lost for normalization
+            'ssp' : 'ssp585'
+            },
     'exp7':
         {'target':'eobs',
-        'domain': [-6., 10., 38, 54],
-        'data_projection' : ccrs.PlateCarree(),
-        'fig_projection' : ccrs.LambertConformal(central_latitude=46., central_longitude=2.),
-        'pyproj_projection' : None, # for curvilign grids conservative interpolation
-        'shape': (64,64),
-        'target_file' : TARGET_EOBS_FRANCE_FILE, # target grid coordinates
-        'orog_file' : OROG_EOBS_FRANCE_FILE,
-        'dataset' : DATASET_EXP7_30Y_DIR,
-        'target_vars': ['pr'],
-        'input_vars': ['elevation', 'huss', 'psl', 'tas'],
-        'channels': ['elevation', 'huss input', 'psl input', 'tas input', 'pr target'], # to not get lost for normalization
-        'ssp' : 'ssp585'   
-        },
+            'domain': [-6., 10., 38, 54],
+            'data_projection' : ccrs.PlateCarree(),
+            'fig_projection' : ccrs.LambertConformal(central_latitude=46., central_longitude=2.),
+            'pyproj_projection' : None, # for curvilign grids conservative interpolation
+            'shape': (64,64),
+            'target_file' : TARGET_EOBS_FRANCE_FILE, # target grid coordinates
+            'orog_file' : OROG_EOBS_FRANCE_FILE,
+            'dataset' : DATASET_EXP7_30Y_DIR,
+            'target_vars': ['pr'],
+            'input_vars': ['elevation', 'huss', 'psl', 'tas'],
+            'channels': ['elevation', 'huss input', 'psl input', 'tas input', 'pr target'], # to not get lost for normalization
+            'ssp' : 'ssp585'   
+            },
     'exp8':
         {'target':'eobs',
-        'domain': [-6., 10., 38, 54],
-        'data_projection' : ccrs.PlateCarree(),
-        'fig_projection' : ccrs.LambertConformal(central_latitude=46., central_longitude=2.),
-        'pyproj_projection' : None, # for curvilign grids conservative interpolation
-        'shape': (64,64),
-        'target_file' : TARGET_EOBS_FRANCE_FILE, # target grid coordinates
-        'orog_file' : OROG_EOBS_FRANCE_FILE,
-        'dataset' : DATASET_EXP8_30Y_DIR,
-        'target_vars': ['pr'],
-        'input_vars': ['elevation',
-                       'zg500',
-                       'zg700',
-                       'zg850',
-                       'ta500',
-                       'ta700',
-                       'ta850',
-                       'ua500',
-                       'ua700',
-                       'ua850',
-                       'vas',
-                       'uas',
-                       'psl'],
-        'channels': ['elevation',
-                       'zg500 input',
-                       'zg700 input',
-                       'zg850 input',
-                       'ta500 input',
-                       'ta700 input',
-                       'ta850 input',
-                       'ua500 input',
-                       'ua700 input',
-                       'ua850 input',
-                       'vas input',
-                       'uas input',
-                       'psl input',
-                       'pr target'], # to not get lost for normalization
-        'ssp' : 'ssp585'   
-        }
+            'domain': [-6., 10., 38, 54],
+            'data_projection' : ccrs.PlateCarree(),
+            'fig_projection' : ccrs.LambertConformal(central_latitude=46., central_longitude=2.),
+            'pyproj_projection' : None, # for curvilign grids conservative interpolation
+            'shape': (64,64),
+            'target_file' : TARGET_EOBS_FRANCE_FILE, # target grid coordinates
+            'orog_file' : OROG_EOBS_FRANCE_FILE,
+            'dataset' : DATASET_EXP8_30Y_DIR,
+            'target_vars': ['pr'],
+            'input_vars': ['elevation',
+                        'zg500',
+                        'zg700',
+                        'zg850',
+                        'ta500',
+                        'ta700',
+                        'ta850',
+                        'ua500',
+                        'ua700',
+                        'ua850',
+                        'vas',
+                        'uas',
+                        'psl'],
+            'channels': ['elevation',
+                        'zg500 input',
+                        'zg700 input',
+                        'zg850 input',
+                        'ta500 input',
+                        'ta700 input',
+                        'ta850 input',
+                        'ua500 input',
+                        'ua700 input',
+                        'ua850 input',
+                        'vas input',
+                        'uas input',
+                        'psl input',
+                        'pr target'], # to not get lost for normalization
+            'ssp' : 'ssp585'   
+            }
     }
 
 COLORS = {'SAFRAN 8km': 'purple',
@@ -202,8 +202,9 @@ SAFRAN_PROJ_PYPROJ = pyproj.Proj(
 
 
 # Phase 1 settings
-DATES = pd.date_range(start='19961231', end='2014-12-31', freq='D')
-DATES_TRAIN = ['1985', '2005', '2010'] # train, valid, test start
+DATES = pd.date_range(start='19850101', end='2004-12-31', freq='D')
+DATES_TRAIN = ['1985', '2001', '2003'] # train, valid, test start (ex8 mini dataset fior test)
+#DATES_TRAIN = ['1985', '2004', '2010'] # train, valid, test start
 DATES_TEST = pd.date_range(start='2010-01-01', end='2014-12-31', freq='D') 
 
 # Phase 2 settings
