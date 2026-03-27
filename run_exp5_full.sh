814d489f4592ba313f1f71f34f8f65e03facfede
page <page@cerfacs.fr>
2026-03-27 15:56:10 +0100

    FIX: Use /gpfs-calypso/home/globc/page in CONDA_PREFIX for robust cross-node resolution

--- run_exp5_full.sh
+++ run_exp5_full.sh
@@ -21,7 +21,7 @@ cd /scratch/globc/page/idownscale_active || exit 1
 
 module load python/anaconda3.11_arm
 
-CONDA_PREFIX="/gpfs-calypso/home/globc/page/.conda/envs/idownscale_env"
+CONDA_PREFIX="$HOME/.conda/envs/idownscale_env"
 PYTHON="$CONDA_PREFIX/bin/python"
 
 # Isolate from system Anaconda (PYTHONHOME) and stale ~/.local packages
